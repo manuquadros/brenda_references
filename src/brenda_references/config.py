5231fc5553ac9cbf72f34c67e53ef2e8e8d8351b
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-11-11 17:08:17 +0100

    fix: compute the right path of config.toml

--- src/brenda_references/config.py
+++ src/brenda_references/config.py
@@ -1,4 +1,9 @@
 import tomllib
+from pathlib import Path
 
 with open("config.toml", mode="rb") as cf:
     config = tomllib.load(cf)
+
+config["documents"] = (
+    Path(__file__).absolute().parent.parent.parent / config["documents"]
+)
