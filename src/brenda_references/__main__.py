8797a752bdcb90d29dd03c791efb24220dbb7ce2
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-11-15 14:18:20 +0100

    feat: retrieve pmc_id, doi and full-text access information

--- src/brenda_references/__main__.py
+++ src/brenda_references/__main__.py
@@ -7,9 +7,40 @@ from brenda_references import db
 from brenda_references.config import config
 from brenda_references.brenda_types import Document, Organism, EC
 
+from ncbi import get_article_ids, is_pmc_open
 
-def complete_doc(doc: Document) -> Document:
-    return doc
+try:
+    api_key = os.environ["NCBI_API_KEY"]
+except KeyError:
+    print(
+        "Continuing without API key. If you want to go faster, set the ",
+        "NCBI_API_KEY environment variable.",
+    )
+
+
+def expand_doc(doc: Document) -> Document:
+    """Check if we can find a PMCID and a DOI for the article."""
+    if not doc.pubmed_id:
+        return doc
+
+    article_ids = get_article_ids(doc.pubmed_id)
+    pmc_id = article_ids.get("pmc")
+
+    if isinstance(pmc_id, str):
+        pmc_id = pmc_id.replace("PMC", "")
+
+    return doc.model_copy(
+        update={
+            "doi": article_ids.get("doi"),
+            "pmc_id": pmc_id,
+            "pmc_open": is_pmc_open(article_ids.get("pmc")),
+        }
+    )
+
+
+def expand_organism(organism: Organism) -> Organism:
+    """Retrieve available synonyms for `organism`"""
+    pass
 
 
 def sync_doc_db():
@@ -41,11 +72,12 @@ def sync_doc_db():
         strain_id = record.Protein_Connect.protein_organism_strain_id
 
         if doc_id not in documents:
-            doc = Document.parse_obj(record._Reference.model_dump())
-            doc = complete_doc(doc)
+            doc = Document.model_validate(record._Reference.model_dump())
+            doc = expand_doc(doc)
             documents[doc_id] = doc.model_dump()
 
         if ec_id not in entities:
+            synonyms = get_ec_synonyms(record._EC.ec_class_id)
             ecs[ec_id] = EC.parse_obj(record._EC.model_dump()).model_dump(
                 exclude={"ec_class_id"}
             )
@@ -58,6 +90,8 @@ def sync_doc_db():
         if strain_id and strain_id not in strains:
             strains.append(strain_id)
 
-    with open(config["documents"], "w") as docs:
-        print(store)
-        json.dump(store, docs, indent=4)
+    print(store)
+
+    # with open(config["documents"], "w") as docs:
+    #     print(store)
+    #     json.dump(store, docs, indent=4)
