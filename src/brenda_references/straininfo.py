4bba0be74a504a50dbe87d79f8be7c48277c1abe
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-11-29 09:49:17 +0100

    style: formatting

--- src/brenda_references/straininfo.py
+++ src/brenda_references/straininfo.py
@@ -1,12 +1,13 @@
-import requests
-from log import logger
-from typing import Any, cast
 from collections.abc import Iterable, Sequence
 from functools import singledispatch
-from pydantic import ValidationError
-from debug import print
-from .brenda_types import Strain
+from typing import Any, cast
 
+import requests
+from debug import print
+from log import logger
+from pydantic import ValidationError
+
+from .brenda_types import Strain
 
 api_root = "https://api.straininfo.dsmz.de/v1/"
 
@@ -64,18 +65,18 @@ def get_strain_ids(query: str | Sequence[str]) -> list[int]:
     return []
 
 
-def get_strain_data(query: int | Sequence[int]) -> Iterable[Strain]:
+def get_strain_data(query: int | Sequence[int]) -> set[Strain]:
     data = cast(list[dict], response(strain_info_api_url(query))) if query else []
 
     try:
-        return (
+        return {
             Strain(
                 **item["strain"],
                 cultures=item["strain"]["relation"].get("culture", frozenset()),
                 designations=item["strain"]["relation"].get("designation", frozenset()),
             )
             for item in data
-        )
+        }
     except ValidationError as e:
         print(data)
         raise e
