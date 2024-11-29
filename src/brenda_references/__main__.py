8e42bb736df480cc7acda7a38f242ad64b472784
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-11-29 11:18:24 +0100

    fix: serialize Pydantic models with mode="json"

--- src/brenda_references/__main__.py
+++ src/brenda_references/__main__.py
@@ -5,8 +5,9 @@ from ncbi import get_article_ids, is_pmc_open
 from tqdm import tqdm
 
 from brenda_references import db
+from collections.abc import Set
 
-from .brenda_types import EC, Bacteria, Document, Store, BrendaStrain
+from .brenda_types import EC, Bacteria, Document, Store, BrendaStrain, BaseModel
 from .config import config
 from debug import print
 from .lpsn_interface import lpsn_synonyms
@@ -59,7 +60,9 @@ def get_document(docdb: tinydb.TinyDB, reference: db._Reference) -> Document:
     if not doc:
         doc = expand_doc(Document.model_validate(reference.model_dump()))
         docdb.table("documents").insert(
-            tinydb.table.Document(doc.model_dump(), doc_id=reference.reference_id)
+            tinydb.table.Document(
+                doc.model_dump(mode="json"), doc_id=reference.reference_id
+            )
         )
 
     return doc
@@ -67,20 +70,25 @@ def get_document(docdb: tinydb.TinyDB, reference: db._Reference) -> Document:
 
 def enzyme_synonyms(
     docdb: tinydb.TinyDB,
-    synonym_refs: dict[int, tuple[str, int]],
+    synonym_refs_dict: dict[int, tuple[str, int]],
     enzymes: set[EC],
     reference_id: int,
 ) -> dict[int, set[str]]:
     enzymes_in_doc = dict()
 
     for enzyme in enzymes:
+        synonym_refs = synonym_refs_dict[enzyme.id]
         enzymes_in_doc[enzyme.id] = {
             syn[0] for syn in synonym_refs if syn[1] == reference_id
         }
 
-        enzyme = item.model_copy(update={"synonyms": {syn[0] for syn in synonym_refs}})
+        enzyme = enzyme.model_copy(
+            update={"synonyms": {syn[0] for syn in synonym_refs}}
+        )
         docdb.table("enzymes").upsert(
-            tinydb.table.Document(enzyme.model_dump(exclude="id"), doc_id=enzyme.id)
+            tinydb.table.Document(
+                enzyme.model_dump(exclude="id", mode="json"), doc_id=enzyme.id
+            )
         )
 
     return enzymes_in_doc
@@ -95,7 +103,9 @@ def bacteria_synonyms(
         syn_in_doc.setdefault(bac.id, set()).add(bac.organism)
         bac = bac.model_copy(update={"synonyms": lpsn_synonyms(bac.lpsn_id)})
         docdb.table("bacteria").upsert(
-            tinydb.table.Document(bac.model_dump(exclude="id"), doc_id=bac.id)
+            tinydb.table.Document(
+                bac.model_dump(exclude="id", mode="json"), doc_id=bac.id
+            )
         )
 
     return syn_in_doc
@@ -108,7 +118,10 @@ def strain_synonyms(
 
     for strain in strains:
         syn_in_doc.setdefault(strain.id, set()).add(strain.name)
-        straindata = get_strain_data(get_strain_ids(strain.name))
+        straindata = {
+            strain.model_dump(mode="json")
+            for strain in get_strain_data(get_strain_ids(strain.name))
+        }
         docdb.table("strains").upsert(
             tinydb.table.Document(straindata, doc_id=strain.id)
         )
@@ -126,7 +139,7 @@ def sync_doc_db():
             # Collect all organism/enzyme relations annotated for the document
             relations = db.brenda_enzyme_relations(db_engine, reference.reference_id)
             ec_syn_refs = {
-                enzyme.id: db.ec_synonyms(db_engine, enzime.id)
+                enzyme.id: db.ec_synonyms(db_engine, enzyme.id)
                 for enzyme in relations["enzymes"]
             }
 
@@ -145,5 +158,7 @@ def sync_doc_db():
             )
 
             docdb.table("documents").upsert(
-                tinydb.table.Document(doc.model_dump(), doc_id=reference.reference_id)
+                tinydb.table.Document(
+                    doc.model_dump(mode="json"), doc_id=reference.reference_id
+                )
             )
