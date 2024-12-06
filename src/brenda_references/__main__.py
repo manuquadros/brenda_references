b47ac73d0947787f53ca00604ca348d4ce3ff24f
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-12-06 16:36:40 +0100

    docs: docstrings

--- src/brenda_references/__main__.py
+++ src/brenda_references/__main__.py
@@ -60,6 +60,7 @@ def expand_doc(ncbi: NCBIAdapter, doc: Document) -> Document:
 
 
 def get_document(docdb: tinydb.TinyDB, reference: db._Reference) -> Document:
+    """Retrieve document from the JSON database by reference_id."""
     doc = docdb.table("documents").get(doc_id=reference.reference_id)
 
     if doc is None:
@@ -71,6 +72,15 @@ def get_document(docdb: tinydb.TinyDB, reference: db._Reference) -> Document:
 def add_document(
     docdb: tinydb.TinyDB, ncbi: NCBIAdapter, reference: db._Reference
 ) -> Document:
+    """Add document metadata to the JSON database after retrieving additional
+    data from NCBI.
+
+    :param docdb: The JSON database
+    :param ncbi: The API adapter connecting to NCBI
+    :param reference: SQLModel containing the initial metadata retrieved from BRENDA.
+
+    :return: Document model containing all the metadata retrieved.
+    """
     doc = expand_doc(ncbi, Document.model_validate(reference.model_dump()))
     docdb.table("documents").insert(
         tinydb.table.Document(
@@ -83,10 +93,23 @@ def add_document(
 
 def enzyme_synonyms(
     docdb: tinydb.TinyDB,
-    synonym_refs_dict: dict[int, tuple[str, int]],
+    synonym_refs_dict: dict[int, list[tuple[str, int]]],
     enzymes: set[EC],
     reference_id: int,
 ) -> dict[int, set[str]]:
+    """Store enzyme data in the JSON database and return the subset of enzyme
+    synonyms that appear in the document referenced by `reference_id`.
+
+    :param docdb: The JSON database
+    :param synonym_refs_dict: dict relating BRENDA EC class ids to a list of
+                              (synonym, reference_id) pairs linking each synonym
+                              to the BRENDA reference in which it was attested
+    :param enzymes: EC classes linked to `reference_id`
+    :param reference_id: ID of the document in the BRENDA database
+
+    :return: dict keyed by EC Class ID, with values corresponding to their
+             respective synonyms attested in `reference_id`.
+    """
     enzymes_in_doc = {}
 
     for enzyme in enzymes:
@@ -110,6 +133,15 @@ def enzyme_synonyms(
 def bacteria_synonyms(
     docdb: tinydb.TinyDB, bacteria: set[Bacteria]
 ) -> dict[int, set[str]]:
+    """Retrieve bacterial synonyms from LPSN and store the resulting `Bacteria`
+    models in the JSON database.
+
+    :param docdb: The JSON database
+    :param bacteria: Set of Bacteria models to be completed with synonyms
+
+    :return: dict keyed by bacteria ID, containing each of the organism's
+             synonyms that were attested in the document.
+    """
     syn_in_doc: dict[int, set[str]] = {}
 
     for bac in bacteria:
@@ -126,12 +158,25 @@ def bacteria_synonyms(
 
 @lru_cache(maxsize=1024)
 def known_designation(docdb: TinyDB, designation: str) -> bool:
+    """Check whether `designation` has been attested in any of the documents
+    in `docdb`."""
     return docdb.table("documents").contains(
         Query().strains.test(lambda attested: designation in attested)
     )
 
 
 def sync_doc_db() -> None:
+    """Make sure that the references present in BRENDA are processed and
+    reflected in the JSON database.
+
+    For each reference, store into the JSON database the entities that are
+    linked to it in BRENDA, as well as the relations between these entities
+    that are annotated in the database.
+
+    At this point, we are not performing any checks as to whether information
+    on BRENDA has changed since the last time we visited it, except as to
+    whether new references were added to it.
+    """
     db_engine = db.get_engine()
 
     with (
