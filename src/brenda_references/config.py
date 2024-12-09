cb17095213b20707ba039f3df1b9c1272e97f8ce
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2024-12-09 11:51:28 +0100

    build: add ROOT_DIR constant to config.py

--- src/brenda_references/config.py
+++ src/brenda_references/config.py
@@ -1,9 +1,12 @@
 import tomllib
 from pathlib import Path
 
-with open("config.toml", mode="rb") as cf:
+ROOT_DIR = Path(__file__).absolute().parent.parent.parent
+
+with open(ROOT_DIR / "config.toml", mode="rb") as cf:
     config = tomllib.load(cf)
 
-config["documents"] = (
-    Path(__file__).absolute().parent.parent.parent / config["documents"]
-)
+config["documents"] = ROOT_DIR / config["documents"]
+
+for resource in config["sources"]:
+    config["sources"][resource] = ROOT_DIR / config["sources"][resource]
