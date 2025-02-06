95e33b22d20506975d5ac5ef4e9b9f6b411a8e91
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2025-02-06 13:17:15 +0100

    fix: rate limit requests in APIAdapter

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,10 +1,11 @@
 import time
 from functools import wraps
-from typing import Callable, Self, Any
+from typing import Any, Callable, Self
+from asyncio import Semaphore, sleep
 
-from retrying import retry
 import httpx
 from aiotinydb.middleware import AIOMiddlewareMixin
+from retrying import retry
 from tinydb.middlewares import CachingMiddleware as SyncCachingMiddleware
 
 
@@ -39,17 +40,29 @@ class APIAdapter:
     Subclasses can initialize the headers parameter of the parent.
     """
 
-    def __init__(self, headers: dict[str, str] = {}) -> None:
+    def __init__(self, headers: dict[str, str] = {}, rate_limit: int = 3) -> None:
         self.client = httpx.AsyncClient(
             headers=headers, timeout=30.0, follow_redirects=True
         )
+        self.semaphore = Semaphore(rate_limit)
+        self.last_request_time = {}
+        self.min_delay = 0.4
 
     async def __aenter__(self) -> Self:
         return self
 
     async def __aexit__(self, exc_type, exc_value, exc_tb) -> None:
-        self.client.aclose()
+        await self.client.aclose()
 
     @retry_if_too_many_requests
     async def request(self, url: str) -> Any:
-        return await self.client.get(url)
+        domain = str(httpx.URL(url).host)
+
+        async with self.semaphore:
+            now = time.time()
+            last_req = self.last_request_time.get(domain, 0)
+            if now - last_req < self.min_delay:
+                await sleep(self.min_delay - (now - last_req))
+            self.last_request_time[domain] = time.time()
+
+            return await self.client.get(url)
