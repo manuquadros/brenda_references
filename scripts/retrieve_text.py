0b4f2c70ec9e8f1a9078012e4142a713782e229e
Emanuel Quadros <emanuel.quadros@dsmz.de>
2025-04-03 17:19:12 +0200

    refactor: Migrate retrieve_text.py to async with AIOTinyDB and improved error handling

--- scripts/retrieve_text.py
+++ scripts/retrieve_text.py
@@ -4,21 +4,20 @@ Feed the document database with abstracts  and full text retrieved from PubMed.
 """
 
 import asyncio
-from collections.abc import Iterable, Iterator, MutableSet
+from collections.abc import Iterable, Iterator, MutableMapping
 from types import TracebackType
 from typing import Self
 
+from aiotinydb import AIOTinyDB
+from aiotinydb.storage import AIOJSONStorage
 from brenda_references.brenda_types import Document
 from brenda_references.config import config
 from ncbi import NCBIAdapter
-from tinydb import TinyDB
-from tinydb.middlewares import CachingMiddleware
-from tinydb.storages import JSONStorage
 from tqdm import tqdm
-from utils import APIAdapter
+from utils import APIAdapter, CachingMiddleware
 
 
-class Missing(MutableSet):
+class Missing(MutableMapping):
     """Context manager set of article ids
 
     Instances of the class will periodically retrieve data about its member ids
@@ -27,7 +26,8 @@ class Missing(MutableSet):
 
     def __init__(
         self,
-        docdb: TinyDB,
+        docdb: AIOTinyDB,
+        api: APIAdapter,
         elems: Iterable[tuple[str, Document]] = (),
         batch_size: int = 250,
     ) -> None:
@@ -37,130 +37,142 @@ class Missing(MutableSet):
         :param batch_size: Size to which the set is allowed to grow before the
             data is flushed.
         """
-        self._set = set(elems)
+        self._dict = dict(elems)
         self.batch_size = batch_size
         self._docdb = docdb
+        self._api = api
 
-    def __repr__(self):
-        return self._set.__repr__()
+    def __repr__(self) -> None:  # noqa: D105
+        return self._dict.__repr__()
 
-    def __contains__(self, elem: tuple[str, Document]) -> bool:
-        return elem in self._set
-
-    def __len__(self) -> int:
-        return len(self._set)
-
-    def __iter__(self) -> Iterator:
-        return iter(self._set)
-
-    def __enter__(self) -> Self:  # noqa: D105
+    async def __aenter__(self) -> Self:  # noqa: D105
         return self
 
-    def __exit__(  # noqa: D105
+    async def __aexit__(  # noqa: D105
         self,
         exc_type: type[BaseException] | None,
         exc_value: BaseException | None,
         exc_tb: TracebackType | None,
     ) -> None:
         if hasattr(self, "flush"):
-            self.flush()
+            await self.flush()
 
-    def add(self, elem: tuple[str, Document]) -> None:
-        """Add `elem` to set and flush if `batch_size` is reached."""
-        print(elem)
-        self._set.add(elem)
+    def __len__(self) -> int:  # noqa: D105
+        return len(self._dict)
 
-        if len(self) == self.batch_size:
-            if hasattr(self, "flush"):
-                self.flush()
+    def __iter__(self) -> Iterator:  # noqa: D105
+        return iter(self._dict)
 
-            # In any case, we remove the documents already processed
-            # from the buffer.
-            self.clear()
+    def __getitem__(self, key: str) -> Document:  # noqa: D105
+        return self._dict[key]
 
-    def discard(self, elem: tuple[str, Document]) -> None:  # noqa: D102
-        self._set.discard(elem)
+    def __setitem__(self, key: str, value: Document) -> None:
+        """__setitem__ method"""
+        self._dict[key] = value
 
-    def _store_in_db(self) -> None:
+    async def set(self, key: str, value: Document) -> None:
+        """Asynchronous wrapper over __setitem__, tracking `self.batch_size`"""
+        self[key] = value
+
+        print(hasattr(self, "flush"), len(self))
+        if hasattr(self, "flush") and len(self) >= self.batch_size:
+            print("flushing")
+            await self.flush()
+
+    def __delitem__(self, key: str) -> None:  # noqa: D105
+        self._dict.__delitem__(key)
+
+    async def _store_in_db(self) -> None:
         """Store the updated documents into the instance's document database."""
-        for key, doc in self:
-            self._docdb.table("documents").update(
+        for key, doc in self.items():
+            await self._docdb.table("documents").update(
                 doc.model_dump(),
                 doc_ids=[key],
             )
+
+    async def flush(self) -> None:
+        """Retrieve the relevant data from NCBI and store it in the database.
+
+        The field to be retrieved is controlled by `self._field`, the retrieval
+        function is self._fetch, and the appropriate NCBI id for retrieval is
+        self._ncbi_id,
+        """
+        retrieved = await self._fetch(
+            getattr(doc, self._ncbi_id)
+            for doc in self.values()
+            if hasattr(doc, self._ncbi_id)
+        )
+
+        for doc_id, doc in self.items():
+            if hasattr(doc, self._ncbi_id):
+                self[doc_id] = doc.model_copy(
+                    update={
+                        self._field: retrieved.get(getattr(doc, self._ncbi_id)),
+                    },
+                )
+
+        self._store_in_db()
+
+        # We remove the documents already processed
+        # from the buffer.
+        self.clear()
 
 
 class MissingAbstract(Missing):
     """Context manager Set for documents without an abstract in record."""
 
-    def __init__(
+    def __init__(  # noqa: D107
         self,
-        docdb: TinyDB,
+        docdb: AIOTinyDB,
         api: APIAdapter,
         elems: Iterable[tuple[str, Document]] = (),
-    ):
-        super().__init__(docdb, elems)
-        self.api = api
-
-    def flush(self):
-        abstracts = asyncio.run(
-            self.api.fetch_ncbi_abstracts(
-                doc.pubmed_id for _, doc in self if doc.pubmed_id
-            ),
-        )
-
-        for _, doc in self:
-            if doc.pubmed_id:
-                doc.abstract = abstracts.get(doc.pubmed_id)
-
-        self._store_in_db()
+    ) -> None:
+        super().__init__(docdb, api, elems)
+        self._ncbi_id = "pubmed_id"
+        self._field = "abstract"
+        self._fetch = self._api.fetch_ncbi_abstracts
 
 
 class MissingFullText(Missing):
     """Context manager Set for documents without a full text in record."""
 
-    def __init__(
+    def __init__(  # noqa: D107
         self,
-        docdb: TinyDB,
+        docdb: AIOTinyDB,
         api: APIAdapter,
         elems: Iterable[tuple[str, Document]] = (),
-    ):
-        super().__init__(docdb, elems)
-        self.api = api
-
-    def flush(self):
-        fulltext = asyncio.run(
-            self.api.fetch_fulltext(
-                doc.pmc_id
-                for _, doc in self
-                if doc.pmc_id and doc.pmc_open is True
-            ),
-        )
-
-        for _, doc in self.validate_docs.values():
-            if doc.pmc_id:
-                doc = doc.model_copy(
-                    update={"fulltext": fulltext.get(doc.pmc_id)}
-                )
-
-        self._store_in_db()
+    ) -> None:
+        super().__init__(docdb, api, elems)
+        self._ncbi_id = "pmc_id"
+        self._field = "fulltext"
+        self._fetch = self._api.fetch_fulltext
 
 
 async def run() -> None:  # noqa: D103
-    with TinyDB(
-        config["documents"],
-        storage=CachingMiddleware(JSONStorage),
-    ) as docdb:
-        async with NCBIAdapter() as ncbi:
-            missing_abstract = MissingAbstract(docdb=docdb, api=ncbi)
-            missing_fulltext = MissingFullText(docdb=docdb, api=ncbi)
+    async with (
+        AIOTinyDB(
+            config["documents"],
+            storage=CachingMiddleware(AIOJSONStorage),
+        ) as docdb,
+        NCBIAdapter() as ncbi,
+        MissingAbstract(docdb=docdb, api=ncbi) as missing_abstract,
+        MissingFullText(docdb=docdb, api=ncbi) as missing_fulltext,
+        asyncio.TaskGroup() as tg,
+    ):
+        for doc in tqdm(docdb.table("documents")):
+            if doc.get("pubmed_id") and not doc.get("abstract", []):
+                tg.create_task(
+                    missing_abstract.set(doc.doc_id, Document.validate(doc)),
+                )
+            if (
+                doc.get("pmc_id")
+                and doc.get("pmc_open")
+                and not doc.get("fulltext", [])
+            ):
+                tg.create_task(
+                    missing_fulltext.set(doc.doc_id, Document.validate(doc)),
+                )
 
-            for doc in tqdm(docdb.table("documents")):
-                if doc.get("pubmed_id") and not doc.get("abstract", []):
-                    missing_abstract.add((doc.doc_id, Document.validate(doc)))
-                if doc.get("pmc_id") and not doc.get("fulltext", []):
-                    missing_fulltext.add((doc.doc_id, Document.validate(doc)))
 
-
-def main() -> None:
+def main() -> None:  # noqa: D103
     asyncio.run(run())
