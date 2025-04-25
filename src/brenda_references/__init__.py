fdfa02403c46ae8a12f15e201c1a9647dbcc62df
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2025-04-25 11:29:23 +0200

    chore: clean __init__.py: remove icecream

--- src/brenda_references/__init__.py
+++ src/brenda_references/__init__.py
@@ -1,12 +1,8 @@
 import stackprinter
-from icecream import ic, install
 
 from .brenda_references import add_abstracts, expand_doc, sync_doc_db
 from .sampling import relation_records
 
 __all__ = ["add_abstracts", "expand_doc", "sync_doc_db"]
-
-ic.configureOutput(includeContext=True)
-install()
 
 stackprinter.set_excepthook(style="darkbg2")
