b5d4d7b88e947e66cabbac408d8710fd0fe763bc
Emanuel Quadros <emanuel.souza.de.quadros@dsmz.de>
2025-05-08 16:46:40 +0200

    build: export dataset splits

--- src/brenda_references/__init__.py
+++ src/brenda_references/__init__.py
@@ -1,11 +1,26 @@
 import pandas as pd
 import stackprinter
 
-from .brenda_references import add_abstracts, expand_doc, sync_doc_db
+from .brenda_references import (
+    add_abstracts,
+    expand_doc,
+    sync_doc_db,
+    validation_data,
+    training_data,
+    test_data,
+)
 from .sampling import relation_records
 
 pd.options.mode.copy_on_write = True
 
-__all__ = ["add_abstracts", "expand_doc", "sync_doc_db", "relation_records"]
+__all__ = [
+    "add_abstracts",
+    "expand_doc",
+    "sync_doc_db",
+    "relation_records",
+    "validation_data",
+    "training_data",
+    "test_data",
+]
 
 stackprinter.set_excepthook(style="darkbg2")
